bb6e54f8d8f464a6aae023cc10e532319c75f2c0
Madanhalakatti <153990042+Madanhalakatti@users.noreply.github.com>
2026-08-04 14:33:57 +0530

    Add files via upload

--- main[1].c
+++ main[1].c
@@ -8,30 +8,22 @@ Code, Compile, Run and Debug online from anywhere in world.
 *******************************************************************************/
 #include <stdio.h>
 
-int main() {
-    int signal_input;
+void display_adc(int adc[], int size)
+{
+    int i;
 
-
-    printf("Traffic Signal Controller\n");
-    printf("1 - RED\n");
-    printf("2 - YELLOW\n");
-    printf("3 - GREEN\n");
-    printf("Enter your choice (1-3): ");
-    scanf("%d", &signal_input);
-    switch (signal_input) {
-        case 1:
-            printf("\n[ SIGNAL STATUS: RED ] -> STOP!\n");
-            break;
-        case 2:
-            printf("\n[ SIGNAL STATUS: YELLOW ] -> CAUTION! Prepare to stop or proceed slowly.\n");
-            break;
-        case 3:
-            printf("\n[ SIGNAL STATUS: GREEN ] -> GO! The path is clear.\n");
-            break;
-        default:
-            printf("\n[ ERROR ] Invalid input! Please enter a number between 1 and 3.\n");
-            break;
+    printf("ADC Readings \n");
+    for(i = 0; i < size; i++)
+    {
+        printf("adc[%d] = %d\n", i, adc[i]);
     }
+}
+
+int main()
+{
+    int adc[5] = {100, 250, 512, 700, 900};
+
+    display_adc(adc, 5);
 
     return 0;
 }
